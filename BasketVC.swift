1d00f5be97ec6b035bb7f9de653c84bebbac21a8
Girlka2810 <kereeevkirill@gmail.com>
2021-03-06 23:32:51 +0300

    Setup App Icon

---
+++ BasketVC.swift
@@ -0,0 +1,14 @@
+//
+//  BasketVC.swift
+//  Music Shop
+//
+//  Created by Кирилл Киреев on 06.03.2021.
+//
+
+import UIKit
+
+class BasketVC: NSObject {
+
+    @IBOutlet weak var messegeFroClient: UITextField!
+    
+}
